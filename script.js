c82b5ea60ebf7913193dc8442b63bf98d537499b
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-09-17 16:07:25 -0300

    selecionando as cores do html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Definindo as orderns. */
+
 let order = []; // Ordens do jogo.
 let clickedOrder = []; // Ordem de cliques do jogador.
 let score = 0; // Para saber quando errou.
@@ -8,3 +10,12 @@ let score = 0; // Para saber quando errou.
  * 2 - Amarelo; e
  * 3 - Azul.
  */
+
+ /* Selecionando as cores do HTML */
+
+ const blue = document.querySelector('.blue');
+ const red = document.querySelector('.red');
+ const green = document.querySelector('green');
+ const yellow = document.querySelector('yellow');
+
+ 
