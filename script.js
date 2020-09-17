38a0c7890ff11bf7c91935d6e4f8492b63a443fe
Lucas <lucas.ti@rodoviaria-poa.com.br>
2020-09-17 16:01:41 -0300

    definindo as ordens

---
+++ script.js
@@ -0,0 +1,10 @@
+let order = []; // Ordens do jogo.
+let clickedOrder = []; // Ordem de cliques do jogador.
+let score = 0; // Para saber quando errou.
+
+/*
+ * 0 - Verde;
+ * 1 - Vermelho;
+ * 2 - Amarelo; e
+ * 3 - Azul.
+ */
