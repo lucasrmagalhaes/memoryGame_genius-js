77ae4fd869376809f8c9a92f0e0fcf0391354a93
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-12-26 08:37:01 -0300

    revisando

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-/* Definindo as orderns. */
+// Definindo as orderns.
 
 let order = []; // Ordens do jogo.
 let clickedOrder = []; // Ordem de cliques do jogador.
 let score = 0; // Para saber quando errou.
 
-/*
+/**
  * 0 - Verde;
  * 1 - Vermelho;
  * 2 - Amarelo; e
  * 3 - Azul.
  */
 
- /* Selecionando as cores do HTML */
+ // Selecionando as cores do HTML
 
  const blue = document.querySelector('.blue');
  const red = document.querySelector('.red');
